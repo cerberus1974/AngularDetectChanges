187e001e6a53b70798369189e7a226dc734ee408
t_takano <cerberus1974@gmail.com>
2020-11-14 10:27:27 +0900

    add component

---
+++ src/app/component3/component3.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  template: `
+    <p>
+      component3 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Component3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
